d08d885ddb8b519dd53a5a24b085655746cb4f7e
aden9460 <qw946032590@163.com>
2023-06-02 14:14:07 +0800

    master

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='torchpruner',
+      version='0.1',
+      description='On-the-fly Structured Pruning for PyTorch',
+      url='https://github.com/marcoancona/TorchPruner',
+      author='Marco Ancona (ETH Zurich)',
+      author_email='marco.ancona@inf.ethz.ch',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'numpy',
+            'scipy',
+            'matplotlib',
+            'thop'
+      ],
+      extras_require={
+            "torch": ["torch>=1.2.0"]
+      },
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
